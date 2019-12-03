8877fb3f1e8154a7a61d949c8b29aa00689c097d
Olena Matokhina <olena@matokhina.com>
2019-12-03 03:09:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,6 @@ def nyc_pigeon_organizer(data)
   data.reduce({}) do |memo, (key1, value)|
     value.reduce(memo) do |inner_memo, (key2, names)|
       names.map do |name|
-        puts "_________________________"
-        puts name
-        puts key1
-        puts key2
-        puts inner_memo
         if inner_memo[name] == nil then 
           inner_memo[name] = {key1 => [key2.to_s]}  
         else
