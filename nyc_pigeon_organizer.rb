1b9ea8cda210f64d50debf3afe0669bd61d3f085
Olena Matokhina <olena@matokhina.com>
2019-12-03 02:45:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,17 @@
 def nyc_pigeon_organizer(data)
   data.reduce({}) do |memo, (key, value)|
-    value.map
+    value.reduce(memo) do |inner_memo, (key, names)|
+      names do |name|
+        puts "_________________________"
+        puts name
+        puts key
+        puts inner_memo
+        
+       # inner_memo.includes?(name) ? 
+      end
+  
+      inner_memo
+    end
     memo 
   end
 end
