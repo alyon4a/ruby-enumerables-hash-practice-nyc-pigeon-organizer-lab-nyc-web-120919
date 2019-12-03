7d7c30c2bf19e6d9a4145d7d7a840309cf3adb9f
Olena Matokhina <olena@matokhina.com>
2019-12-03 02:55:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,8 @@ def nyc_pigeon_organizer(data)
         puts name
         puts key2
         puts inner_memo
-        
-        inner_memo.includes?(name) ? inner_memo[name][key1] << key2 : inner_memo[name][key1] = [key2]
+        inner_memo[name] = {key1: [key2]}  if inner_memo[name] == nil
+        inner_memo[name][key1] << key2 : inner_memo[name][key1] = [key2]
       end
   
       inner_memo
