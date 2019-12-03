ec71b1795186d3e75f51e78873b34edf273b40fb
Olena Matokhina <olena@matokhina.com>
2019-12-03 02:51:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,13 @@
 def nyc_pigeon_organizer(data)
-  data.reduce({}) do |memo, (key, value)|
-    value.reduce(memo) do |inner_memo, (key, names)|
+  data.reduce({}) do |memo, (key1, value)|
+    value.reduce(memo) do |inner_memo, (key2, names)|
       names.map do |name|
         puts "_________________________"
         puts name
-        puts key
+        puts key2
         puts inner_memo
         
-        inner_memo.includes?(name) ? : inner_memo
+        inner_memo.includes?(name) ? inner_memo[name][key1] << key2 : inner_memo[name][key1] = [key2]
       end
   
       inner_memo
