6d25826c63275569878aa7d31904ba16841da6a7
Olena Matokhina <olena@matokhina.com>
2019-12-03 03:11:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.reduce({}) do |memo, (key1, value)|
+    value.reduce(memo) do |inner_memo, (key2, names)|
+      names.map do |name|
+        if inner_memo[name] == nil then 
+          inner_memo[name] = {key1 => [key2.to_s]}  
+        else
+          inner_memo[name][key1] = [] if inner_memo[name][key1] == nil
+          inner_memo[name][key1] << key2.to_s
+        end
+      end
+      inner_memo
+    end
+    memo 
+  end
 end
